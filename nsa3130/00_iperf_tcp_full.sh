c61344b0912de34654d5729e9a0e0f2e0cc75cd4
Namgon Kim <day10000@gmail.com>
2016-06-10 21:24:33 +0900

    Add a new script for iperf tcp with threads

---
+++ nsa3130/00_iperf_tcp_full.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source /etc/profile
+
+NHR=`date '+%Y_%m%d_%H'`
+NOW=`date '+%Y_%m%d_%H%M_%S'`
+
+BOX="nsa3130"
+SVR="211.224.204.206"
+THR="20"
+# MSS 256 ==> about 600Mbps
+MSS=256
+
+LOG_DIR="/var/log/stress/${BOX}/${NHR}"
+mkdir -p $LOG_DIR
+
+#iperf -c ${SVR} -i 1 -P $THR -M $MSS -t 600 &> $LOG_DIR/01_iperf_tcp_full_${NOW}.log
+ip netns exec nsa3130 iperf -c ${SVR} -M $MSS -P $THR -i 1 -t 3600 &> $LOG_DIR/01_iperf_tcp_full_${NOW}.log
