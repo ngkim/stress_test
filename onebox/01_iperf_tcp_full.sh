7f70167d642e9c306e43050edd6a8dff9f68543f
Namgon Kim <day10000@gmail.com>
2016-06-10 21:18:32 +0900

    Add a script for sending traffic with MSS

--- onebox/01_iperf_tcp_full.sh
+++ onebox/01_iperf_tcp_full.sh
@@ -7,8 +7,11 @@ NOW=`date '+%Y_%m%d_%H%M_%S'`
 
 BOX="nsa3130"
 SVR="211.224.204.206"
+THR="20"
+MSS=256
 
 LOG_DIR="/var/log/stress/${BOX}/${NHR}"
 mkdir -p $LOG_DIR
 
-iperf -c ${SVR} -i 1 -t 600 &> $LOG_DIR/01_iperf_tcp_full_${NOW}.log
+#iperf -c ${SVR} -i 1 -P $THR -M $MSS -t 600 &> $LOG_DIR/01_iperf_tcp_full_${NOW}.log
+iperf -c ${SVR} -M $MSS -i 1 -t 600 &> $LOG_DIR/01_iperf_tcp_full_${NOW}.log
